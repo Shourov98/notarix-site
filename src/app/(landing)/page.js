16536c67fa2f30fe485bb6451092f056180096ae
Shourov98 <mdmahbub0412@gmail.com>
2026-05-03 11:49:29 +0600

    feat (landing page): service process section design

--- src/app/(landing)/page.js
+++ src/app/(landing)/page.js
@@ -1,10 +1,12 @@
 import Hero from "@/components/landing_page/Hero";
+import HowItWorks from "@/components/landing_page/HowItWorks";
 import Features from "@/components/landing_page/Features";
 
 export default function Home() {
   return (
     <main>
       <Hero />
+      <HowItWorks />
       <Features />
     </main>
   );
